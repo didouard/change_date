330bbbb964b3666829f4b398e5fd72485e86e357
Edouard FERRARI <eferrari@vente-privee.com>
2019-01-01 18:21:34 +0100

    Set comment

--- Change date.sh
+++ Change date.sh
@@ -1,38 +1,42 @@
 
-date_day=24;
+date_day=29;
 date_month=10;
 date_year=2018;
 
-start_file=3081;
-end_file=3246;
+start_file=3462;
+end_file=3628;
 
 a=0;
 for i in `seq $start_file $end_file`; do
-    
+
+    # Is JPG or MP4 file
     file="none";
     if [ -e GOPR$i.JPG ]; then file=GOPR$i.JPG; fi
     if [ -e GOPR$i.MP4 ]; then file=GOPR$i.MP4; fi
 
-    if [ ! $file == "none" ]; then
-	echo -n "[-] "$file;
+    # If file doesn't exist, we continue
+    if [ $file == "none" ]; then continue; fi
 
-	if [ 10 -gt $(($a % 60)) ]; then
-	    minutes="0"$(($a % 60));
-	else
-	    minutes=$(($a % 60));
-	fi 
+    # We want to set 1 minute for every item
+    echo -n "[-] "$file;
+    if [ 10 -gt $(($a % 60)) ]; then
+	minutes="0"$(($a % 60));
+    else
+	minutes=$(($a % 60));
+    fi 
+    echo " created and modified "$modified_date;
 
-	echo " created and modified "$modified_date;
-	
-	modified_date=$date_year":"$date_month":"$date_day\ $((10 + ($a + 1)/60)):$minutes:00
-	exiftool -DateTimeOriginal="$modified_date" $file;
-	exiftool -ModifyDate="$modified_date" $file;
-	exiftool -CreateDate="$modified_date" $file;
+    # Change metadata
+    modified_date=$date_year":"$date_month":"$date_day\ $((10 + ($a + 1)/60)):$minutes:00
+    exiftool -DateTimeOriginal="$modified_date" $file;
+    exiftool -ModifyDate="$modified_date" $file;
+    exiftool -CreateDate="$modified_date" $file;
 
-	modified_date=$date_month\/$date_day\/$date_year\ $((10 + ($a + 1)/60)):$minutes:00
-	SetFile -d "$modified_date" $file;
-	SetFile -m "$modified_date" $file;
-	
-	a=$((a+1));
-    fi;
+    # Change on filesystem
+    modified_date=$date_month\/$date_day\/$date_year\ $((10 + ($a + 1)/60)):$minutes:00
+    SetFile -d "$modified_date" $file;
+    SetFile -m "$modified_date" $file;
+    
+    a=$((a+1));
+
 done
